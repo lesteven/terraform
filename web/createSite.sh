6ea7eb5032e1c7ec60867c2a11c2a04dadb33780
lesteven <le_steven@outlook.com>
2019-03-24 19:53:28 -0700

    chore: add package install script

--- web/createSite.sh
+++ web/createSite.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# install packages
+
+sudo apt-get update
+printf Y | sudo apt-get install golang-go
+printf Y | sudo apt-get install nginx
+
+# get server code
 git clone https://github.com/lesteven/goServer.git
+
+# setup nginx
+sudo mv example.com /etc/nginx/sites-available/example.com
+sudo ln -s /etc/nginx/sites-available/example.com /etc/nginx/sites-enabled/
+
+sudo sed -i 's/# server_names/server_names/' /etc/nginx/nginx.conf
+
+sudo systemctl restart nginx
