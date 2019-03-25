56f8f4078c4142534abb4ee3912442ffa7c9d705
lesteven <le_steven@outlook.com>
2019-03-24 18:14:02 -0700

    feat: add terraform provisioners

--- web/web.tf
+++ web/web.tf
@@ -9,5 +9,20 @@ resource "aws_instance" "web" {
   ami           = "${var.ami}"
   instance_type = "t2.micro"
   key_name      = "${var.key_name}"
+  provisioner "file" {
+    source = "createSite.sh"
+    destination = "/tmp/createSite.sh"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/createSite.sh",
+      "sudo /tmp/createSite.sh"
+    ]
+  }
+  connection {
+    type = "ssh"
+    user = "${var.user}"
+    private_key = "${file("~/.ssh/${var.key_file}")}"
+  }
 }
 
