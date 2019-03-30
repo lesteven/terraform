858894bfe416ec31f698e359ae574afb95f85881
lesteven <le_steven@outlook.com>
2019-03-30 15:05:11 -0700

    feat: add vpc

--- web/login.sh
+++ web/login.sh
@@ -4,7 +4,7 @@
 source launchWeb.config
 
 # get public_dns 
-public_dns=$(grep public_dns terraform.tfstate | awk '{print $2}' | sed 's/[",]//g')
+public_dns=$(grep -w public_ip terraform.tfstate | awk '{print $2}' | sed 's/[",]//g')
 
 
 ssh -tt -oStrictHostKeyChecking=no -i ~/.ssh/$key_file ubuntu@$public_dns
--- web/web.tf
+++ web/web.tf
@@ -5,10 +5,69 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+resource "aws_vpc" "web_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "webint" {
+  vpc_id = "${aws_vpc.web_vpc.id}"
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id = "${aws_vpc.web_vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.webint.id}"
+}
+
+resource "aws_subnet" "websubnet" {
+  vpc_id = "${aws_vpc.web_vpc.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "web_server" {
+  name = "web_server"
+  description = "allow http and https"
+  vpc_id = "${aws_vpc.web_vpc.id}"
+
+  # ssh
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # https
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  # http
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = "${var.ami}"
   instance_type = "t2.micro"
   key_name      = "${var.key_name}"
+
+  vpc_security_group_ids = ["${aws_security_group.web_server.id}"]
+  subnet_id = "${aws_subnet.websubnet.id}"
+
   provisioner "file" {
     source = "createSite.sh"
     destination = "/tmp/createSite.sh"
