894ae6cd6725bddb79b6d56d2ed8ca99405ba284
lesteven <le_steven@outlook.com>
2019-04-09 00:20:14 -0700

    feat: add ami

---
+++ snap/login.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# source to get variables
+source launchWeb.config
+
+# get public_ip 
+public_ip=$(grep -w public_ip terraform.tfstate | awk '{print $2}' | sed 's/[",]//g')
+
+
+ssh -tt -oStrictHostKeyChecking=no -i ~/.ssh/$key_file ubuntu@$public_ip
--- snap/snap.tf
+++ snap/snap.tf
@@ -18,6 +18,18 @@ resource "aws_ebs_snapshot" "web_snap" {
   volume_id = "${data.aws_ebs_volume.web_vol.id}"
 }
 
+# create ami from snapshot
+resource "aws_ami" "web_ami" {
+  name = "web_ami"
+  virtualization_type = "hvm"
+  root_device_name = "/dev/sda1"
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    snapshot_id = "${aws_ebs_snapshot.web_snap.id}"
+  }
+}
+
 # get security group
 data "aws_security_group" "web_server" {
   filter {
@@ -28,7 +40,7 @@ data "aws_security_group" "web_server" {
 
 # create instance w/ snapshot
 resource "aws_instance" "web2" {
-  ami = "${var.ami}"
+  ami = "${aws_ami.web_ami.id}"
   instance_type = "t2.micro"
   security_groups = ["${data.aws_security_group.web_server.name}"]
   key_name = "${var.key_name}"
