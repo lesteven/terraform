7452ca2004c760632196aafd5b59eba73de86f42
lesteven <le_steven@outlook.com>
2019-04-10 11:43:49 -0700

    feat: add load balancer

--- autoscale/autoscale.tf
+++ autoscale/autoscale.tf
@@ -6,20 +6,33 @@ provider "aws" {
 
 # fetch ami from aws
 data "aws_ami" "web_ami" {
+  owners = ["self"]
   filter {
     name = "name"
     values = ["${var.ami_name}"]
   }
 }
 
+# get security group
+data "aws_security_group" "web_server" {
+  filter {
+    name = "group-name"
+    values = ["${var.security_name}"]
+  }
+}
+
 # fetch instance from aws
 data "aws_instance" "webA" {
-
+  instance_id = "${var.instance_id}"
 }
 
 # create new instance from ami
 resource "aws_instance" "webB" {
-
+  ami = "${data.aws_ami.web_ami.id}"
+  instance_type = "t2.micro"
+  security_groups = ["${data.aws_security_group.web_server.name}"]
+  key_name = "${var.key_name}"
+  availability_zone = "${var.zones[1]}"
 }
 
 #create elastic load balancer w/ two availability zones
@@ -27,7 +40,14 @@ resource "aws_elb" "autoscale" {
   name = "autoscale"
   availability_zones = "${var.zones}"
   listener {
-
+    instance_port = 80
+    instance_protocol = "http" 
+    lb_port = 80
+    lb_protocol = "http"
   }
-  instances = ["${aws_instance.}"]
+  instances = [
+    "${data.aws_instance.webA.id}", 
+    "${aws_instance.webB.id}"
+  ]
 }
+
--- autoscale/variables.tf
+++ autoscale/variables.tf
@@ -2,3 +2,6 @@ variable "region" {}
 variable "cred_file" {}
 variable "ami_name" {}
 variable "zones" { type = "list" }
+variable "instance_id" {}
+variable "security_name" {}
+variable "key_name" {}
