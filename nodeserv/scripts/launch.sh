d66e16b4dd3065069580bed1f7f8debb3e4ff09f
lesteven <le_steven@outlook.com>
2019-04-29 16:29:27 -0700

    chore: update launch script

--- nodeserv/scripts/launch.sh
+++ nodeserv/scripts/launch.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# go to script dir, then one level down
-cd "$(dirname "$0")/.."
+# go to script dir
+cd "$(dirname "$0")"
 
 # clone private repo to copy into ec2
-git clone git@github.com:lesteven/nodeServer.git
-mv nodeServer ./scripts/prov
+git clone git@github.com:lesteven/nodeServer.git ./prov/nodeServer
 
+# go to root to activate terraform
+cd ..
 terraform init
 printf yes | terraform apply
 
