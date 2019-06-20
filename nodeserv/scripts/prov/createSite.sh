be31540111cd897e59e6175ba90b92c63498843e
lesteven <le_steven@outlook.com>
2019-06-19 20:21:42 -0700

    chore: add comments to script

--- nodeserv/scripts/prov/createSite.sh
+++ nodeserv/scripts/prov/createSite.sh
@@ -8,6 +8,8 @@ sudo apt-get install -y nginx
 
 
 # replace example.com w/ ip address
+# "$(dirname "$0")" is the dir where the bashscript exists (createSite)
+# change into this dir, b/c this is where example.com exists as well
 cd "$(dirname "$0")"
 public_ip=$(cat ip.txt)
 sed -i "s/example.com/$public_ip/" ./example.com
