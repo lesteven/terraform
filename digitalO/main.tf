783192371e81af2ab5267dd2f7406c2604419000
lesteven <stevenle2011@gmail.com>
2019-08-24 09:36:50 -0700

    feat: add droplet

--- digitalO/main.tf
+++ digitalO/main.tf
@@ -2,3 +2,9 @@
 provider "digitalocean" {
   token = "${var.do_token}"
 }
+resource "digitalocean_droplet" "server" {
+  image = "ubuntu-18-04-x64"
+  name = "server"
+  region = "sfo2"
+  size = "s-1vcpu-1gb"
+}
