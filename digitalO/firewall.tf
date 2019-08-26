5927dfecb1608a9f6d0ca284fb5fb5f7b7214be3
lesteven <stevenle2011@gmail.com>
2019-08-26 14:05:23 -0700

    feat: add firewall

---
+++ digitalO/firewall.tf
@@ -0,0 +1,45 @@
+resource "digitalocean_firewall" "fw" {
+  name = "web-server"
+
+  droplet_ids = ["${digitalocean_droplet.server.id}"]
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "22"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "80"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "tcp"
+      port_range         = "443"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+      protocol           = "icmp"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "tcp"
+      port_range              = "53"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "udp"
+      port_range              = "53"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+      protocol                = "icmp"
+      destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+}
